16bd598688306d03a7db1aa73c625c2a4b35f9a0
Marc R. Uchniat <logan5@feem.net>
2020-07-25 13:39:38 -0500

    add display

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
-#include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <Adafruit_NeoTrellis.h>
+#include <Wire.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+const int lineHeight = 12;
+const int lineIndent = 4;
+const int lineStart = 12;
 
 Adafruit_seesaw sensor [4];
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 bool sensorStatus[4];
 int baseAddress = 0x36;
 
@@ -9,6 +19,14 @@ void setup() {
     Serial.begin(115200);
     delay(500);
     Serial.println("ONLINE");
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    display.clearDisplay();
+    display.setTextWrap(false);
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(5,15);
+    display.print("AAAAAAAAAA");
+    display.display();
 
     for (int i = 0; i < 4; i++) {
         int a = baseAddress + i;
@@ -27,13 +45,19 @@ void setup() {
 void loop() {
     float tempC[4];
     uint16_t capread[4];
+    display.clearDisplay();
+    display.setTextSize(1.5);
     for (int i = 0; i < 4; i++) {
+        int line =  lineStart + (lineHeight * i);
         tempC[i] = sensor[i].getTemp();
         capread[i] = sensor[i].touchRead(0);
 
+        display.setCursor(lineIndent, line);
+        display.printf("[%i] T: %.1fC M: %i", i, tempC[i], capread[i]);
         Serial.printf("%iT: %fC ", i, tempC[i]);
         Serial.printf("%iC: %i ", i, capread[i]);
     }
+    display.display();
     Serial.println();
     delay(2000);
 }
