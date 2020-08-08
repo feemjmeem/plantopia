41e1e5a83b60abf781639a4b88760a2e5cb782f7
Marc R. Uchniat <logan5@feem.net>
2020-08-08 11:50:09 -0500

    add wifi and ota

---
+++ include/config.h
@@ -0,0 +1,6 @@
+#define WIFI_SSID               "MY_SSID" 
+#define WIFI_PASSWORD           "MY_WIFI_PASSWORD"
+#define HOSTNAME                "plantopia"
+
+#define OTA_PORT                8266
+#define OTA_PASSWORD            "MY_OTA_PASSWORD" // lol no hash
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include <Adafruit_NeoTrellis.h>
+#include <ArduinoOTA.h>
 #include <Wire.h>
+#include "config.h"
+
+const char *ssid = WIFI_SSID;
+const char *wifi_password = WIFI_PASSWORD;
+const char *hostname = HOSTNAME;
+
+const uint16_t ota_port = OTA_PORT;
+const char *ota_password = OTA_PASSWORD;
 
 Adafruit_seesaw sensor [4];
 bool sensorStatus[4];
@@ -8,7 +17,44 @@ int baseAddress = 0x36;
 void setup() {
     Serial.begin(115200);
     delay(500);
-    Serial.println("ONLINE");
+    Serial.printf("\nConnecting...");
+    WiFi.hostname(hostname);
+    WiFi.begin(ssid, wifi_password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(100);
+        Serial.print(".");
+    }
+
+    Serial.printf("Connected!\n");
+
+    ArduinoOTA.setPort(ota_port);
+    ArduinoOTA.setHostname(hostname);
+    ArduinoOTA.setPassword(ota_password);
+
+    ArduinoOTA.onStart([]() {
+        Serial.println("Start updating...");
+    });
+    ArduinoOTA.onEnd([]() {
+        Serial.println("\nEnd");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) {
+            Serial.println("Auth Failed");
+        } else if (error == OTA_BEGIN_ERROR) {
+            Serial.println("Begin Failed");
+        } else if (error == OTA_CONNECT_ERROR) {
+            Serial.println("Connect Failed");
+        } else if (error == OTA_RECEIVE_ERROR) {
+            Serial.println("Receive Failed");
+        } else if (error == OTA_END_ERROR) {
+            Serial.println("End Failed");
+        }
+    });
+    ArduinoOTA.begin();
 
     for (int i = 0; i < 4; i++) {
         int a = baseAddress + i;
@@ -35,5 +81,6 @@ void loop() {
         Serial.printf("%iC: %i ", i, capread[i]);
     }
     Serial.println();
-    delay(500);
+    ArduinoOTA.handle();
+    delay(1000);
 }
