5d35039329cb7100419ac3a26a0472cf4a4182d8
Marc R. Uchniat <logan5@feem.net>
2021-03-21 15:19:18 -0500

    add debug mode, add es args support, functions

--- include/config.h
+++ include/config.h
@@ -8,6 +8,7 @@
 #define ES_HOST                 "MY_ES_HOST"
 #define ES_PORT                 9200
 #define ES_INDEX                "plantopia"
+// #define ES_ARGS                 "" // uncomment to set args after _doc at index time
 
-#define SENSOR_COUNT            4
+#define SENSOR_COUNT            4 // set to 0 for debug
 #define SENSOR_BASEADDRESS      0x36
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 #include <Wire.h>
 #include "config.h"
 
+#ifndef ES_ARGS
+#define ES_ARGS ""
+#endif
+
 const char *ssid = WIFI_SSID;
 const char *wifi_password = WIFI_PASSWORD;
 const char *hostname = HOSTNAME;
@@ -15,6 +19,7 @@ const char *ota_password = OTA_PASSWORD;
 const char *es_host = ES_HOST;
 const uint16_t es_port = ES_PORT;
 const char *es_index = ES_INDEX;
+const char *es_args = ES_ARGS;
 
 Adafruit_seesaw sensor [SENSOR_COUNT];
 bool sensorStatus[SENSOR_COUNT];
@@ -22,6 +27,10 @@ int baseAddress = SENSOR_BASEADDRESS;
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", 0);
+
+unsigned long get_time();
+String create_data(uint16_t cap, uint16_t temp, int sensor, unsigned long time);
+String create_post(String data);
 
 void setup() {
     Serial.begin(115200);
@@ -64,16 +73,17 @@ void setup() {
         }
     });
     ArduinoOTA.begin();
-
-    for (int i = 0; i < SENSOR_COUNT; i++) {
-        int a = baseAddress + i;
-        if (!sensorStatus[i]) {
-            if (!sensor[i].begin(a)) {
-                Serial.printf("NO SENSOR %i\n", i);
-                while(1);
-            } else {
-                Serial.printf("sensor %x started v%x\n", a, sensor[i].getVersion());
-                sensorStatus[i] = true;
+    if(SENSOR_COUNT) {
+        for (int i = 0; i < SENSOR_COUNT; i++) {
+            int a = baseAddress + i;
+            if (!sensorStatus[i]) {
+                if (!sensor[i].begin(a)) {
+                    Serial.printf("NO SENSOR %i\n", i);
+                    while(1);
+                } else {
+                    Serial.printf("sensor %x started v%x\n", a, sensor[i].getVersion());
+                    sensorStatus[i] = true;
+                }
             }
         }
     }
@@ -89,24 +99,27 @@ void loop() {
         Serial.println("Can't connect to elasticsearch");
         return;
     }
+    
+    if (SENSOR_COUNT) {
+        for (int i = 0; i < SENSOR_COUNT; i++) {
+            tempC[i] = sensor[i].getTemp();
+            capread[i] = sensor[i].touchRead(0);
 
-    for (int i = 0; i < SENSOR_COUNT; i++) {
-        timeClient.update();
-        unsigned long time = timeClient.getEpochTime();
-        tempC[i] = sensor[i].getTemp();
-        capread[i] = sensor[i].touchRead(0);
-
-        Serial.printf("%iT: %fC ", i, tempC[i]);
-        Serial.printf("%iC: %i ", i, capread[i]);
-        String data = "{\"capacitance\": " + String(capread[i]) + "," +
-                      "\"sensor\": " + String(i) + "," +
-                      "\"temperature\": " + String(tempC[i]) + "," +
-                      "\"timestamp\": " + String(time) + "}";
-        String post = "POST /" + String(es_index) + "/_doc HTTP/1.1\r\n" + 
-                      "Content-Length: " + data.length() + "\r\n" +
-                      "Content-Type: application/json" + "\r\n" +
-                      "\r\n" +
-                      data;
+            Serial.printf("%iT: %fC ", i, tempC[i]);
+            Serial.printf("%iC: %i ", i, capread[i]);
+            String post = create_post(create_data(capread[i],tempC[i],i,get_time()));
+            Serial.println();
+            Serial.println(post);
+            client.print(post);
+            delay(50);
+            while(client.available()) {
+                String line = client.readStringUntil('\r');
+                Serial.print(line);
+            }
+        }
+    } else {
+        Serial.printf("debug");
+        String post = create_post(create_data(0.0,0.0,0,get_time()));
         Serial.println();
         Serial.println(post);
         client.print(post);
@@ -115,7 +128,30 @@ void loop() {
             String line = client.readStringUntil('\r');
             Serial.print(line);
         }
-    }
+    } 
     ArduinoOTA.handle();
     delay(5000);
+}
+
+unsigned long get_time() {
+    timeClient.update();
+    unsigned long time = timeClient.getEpochTime();
+    return time;
+}
+
+String create_data(uint16_t cap, uint16_t temp, int sensor, unsigned long time) {
+    String data = "{\"capacitance\": " + String(cap) + "," +
+                  "\"sensor\": " + String(sensor) + "," +
+                  "\"temperature\": " + String(temp) + "," +
+                  "\"timestamp\": " + String(time) + "}";
+    return data;
+}
+
+String create_post(String data) {
+    String post = "POST /" + String(es_index) + "/_doc" + String(es_args) + " HTTP/1.1\r\n" +
+                  "Content-Length: " + data.length() + "\r\n" +
+                  "Content-Type: application/json" + "\r\n" +
+                  "\r\n" +
+                  data;
+    return post;
 }
